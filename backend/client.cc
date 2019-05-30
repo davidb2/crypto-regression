d0cf164c6f51333f3d7b93e4e465fbc6dddde506
= <brewcrew5498@outlook.com>
2019-05-29 20:03:51 -0500

    get client code.

--- backend/client.cc
+++ backend/client.cc
@@ -1,41 +1,87 @@
+#include <functional>
+#include <string>
+
 #include "client.h"
 #include "types.h"
 
+using std::bind;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
 namespace gdax {
 
-void Client::onOpen(WebsocketClient* c, WebsocketHandle hdl) {
+
+void Client::onOpen(WebsocketHandle hdl) {
   std::string msg = "Hello";
-  c->send(hdl, msg, websocketpp::frame::opcode::text);
-  c->get_alog().write(websocketpp::log::alevel::app, "Sent Message: " + msg);
+  c_.send(hdl, msg, websocketpp::frame::opcode::text);
+  c_.get_alog().write(websocketpp::log::alevel::app, "Sent Message: " + msg);
 }
 
-void Client::onFail(WebsocketClient* c) {
-  c->get_alog().write(websocketpp::log::alevel::app, "Connection Failed");
+void Client::onFail(WebsocketHandle hdl) {
+  c_.get_alog().write(websocketpp::log::alevel::app, "Connection Failed");
 }
 
-void Client::onMessage(
-  WebsocketClient* c,
-  WebsocketHandle hdl,
-  WebsocketMessagePtr msg)
-{
-  c->get_alog().write(
+void Client::onMessage(WebsocketHandle hdl, WebsocketMessagePtr msg) {
+  c_.get_alog().write(
     websocketpp::log::alevel::app,
     "Received Reply: " + msg->get_payload());
 
   WebsocketErrorCode ec;
-  c->send(hdl, "hi" + msg->get_payload(), msg->get_opcode(), ec);
+  c_.send(hdl, "hi" + msg->get_payload(), msg->get_opcode(), ec);
 
   if (ec) {
-    c->get_alog().write(
+    c_.get_alog().write(
       websocketpp::log::alevel::app,
       "Error: " + ec.message());
 
-    // c->close(hdl,websocketpp::close::status::normal, "");
+    // c_->close(hdl,websocketpp::close::status::normal, "");
   }
 }
 
-void Client::onClose(WebsocketClient* c, WebsocketHandle hdl) {
-  c->get_alog().write(websocketpp::log::alevel::app, "Connection Closed");
+void Client::onClose(WebsocketHandle hdl) {
+  c_.get_alog().write(websocketpp::log::alevel::app, "Connection Closed");
+}
+
+Client::Client(const Server* server) {
+  // Set logging to be pretty verbose (everything except message payloads)
+  c_.clear_access_channels(websocketpp::log::alevel::frame_header);
+  c_.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+  // Initialize ASIO
+  c_.init_asio();
+
+  // Register our message handler
+  c_.set_open_handler(bind(&Client::onOpen, this, ::_1));
+  c_.set_fail_handler(bind(&Client::onFail, this, ::_1));
+  c_.set_message_handler(bind(&Client::onMessage, this, ::_1, ::_2));
+  c_.set_close_handler(bind(&Client::onClose, this, ::_1));
+}
+
+bool Client::start(const std::string& uri, WebsocketErrorCode* eOut) noexcept {
+  WebsocketErrorCode ec;
+  try {
+    WebsocketClient::connection_ptr con = c_.get_connection(uri, ec);
+
+    if (ec) {
+      std::cout << "could not create connection because: " << ec.message() << std::endl;
+      if (eOut) *eOut = ec;
+      return false;
+    }
+
+    // Note that connect here only requests a connection. No network messages are
+    // exchanged until the event loop starts running in the next line.
+    c_.connect(con);
+
+    // Start the ASIO io_service run loop
+    // this will cause a single connection to be made to the server. c.run()
+    // will exit when this connection is closed.
+    c_.run();
+  } catch (const websocketpp::exception& e) {
+    std::cout << e.what() << std::endl;
+    if (eOut) *eOut = ec;
+    return false;
+  }
+  return true;
 }
 
 } // namespace gdax
--- backend/client.h
+++ backend/client.h
@@ -1,3 +1,5 @@
+#include <string>
+
 #include <websocketpp/client.hpp>
 #include <websocketpp/config/asio_no_tls_client.hpp>
 
@@ -12,17 +14,17 @@ typedef websocketpp::config::asio_client::message_type::ptr WebsocketMessagePtr;
 // Gdax Client.
 class Client {
  public:
-  Client(const std::string& uri, const Server* server);
-  void onOpen(WebsocketClient* client, WebsocketHandle handle);
-  void onFail(WebsocketClient* client);
-  void onClose(WebsocketClient* client, WebsocketHandle handle);
-  void onMessage(
-    WebsocketClient* client,
-    WebsocketHandle handle,
-    WebsocketMessagePtr message
-  );
+  Client(const Server* server);
+  bool start(const std::string& uri, WebsocketErrorCode* ec) noexcept;
 
  private:
+  void onOpen(WebsocketHandle handle);
+  void onFail(WebsocketHandle handle);
+  void onClose(WebsocketHandle handle);
+  void onMessage(WebsocketHandle handle, WebsocketMessagePtr message);
+
+ private:
+  WebsocketClient c_;
 };
 
 } // namespace gdax
